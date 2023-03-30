054a2c64bfa642bb28c9fce45909dd05d3cb333b
DESKTOP-ULIPBSS\nemet <nemeth.mark@effixmarketing.eu>
2023-03-30 15:19:34 +0200

    cors

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  
 
 app.post('/login', (req, res) => {
     let loginUser = new User({
