a26954f22abe13a4a3923b4c684c7394ae9218a8
DESKTOP-ULIPBSS\nemet <nemeth.mark@effixmarketing.eu>
2023-03-30 14:50:11 +0200

    changed cors

--- app.js
+++ app.js
@@ -36,7 +36,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "adatbazis.gyorki.hu"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.post('/login', (req, res) => {
     let loginUser = new User({
