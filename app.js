ff870379ec39037af624bf965f942dbda1524dea
Effix <45165639+effix-web@users.noreply.github.com>
2023-03-30 16:58:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+  origin: 'https://adatbazis.gyorki.hu',
+  optionsSuccessStatus: 200
+}));
 
 app.post('/login', (req, res) => {
     let loginUser = new User({
